7012f0519b9cfa559680ded24959a553f43ef5ac
wilsonp <ss321pp076@163.com>
2013-10-15 11:10:46 +0800

    init

---
+++ example/run.js
@@ -0,0 +1,7 @@
+var App = require('../index.js').App;
+var app = new App();
+
+app.use(require('./middle01.js'));
+app.use(require('./middle02.js'));
+
+app.listen(8000);
---
+++ index.js
@@ -0,0 +1,5 @@
+var static = require('./lib/static.js');
+var App = require('./lib/App.js');
+
+exports.static = static;
+exports.App = App;
